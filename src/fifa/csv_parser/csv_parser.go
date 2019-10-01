fbbf4f111ad1dda1286277043cae9b3140a390be
Vijay Kiran <vijaykiran225@gmail.com>
2019-10-01 23:31:46 +0530

    correcting errors

--- src/fifa/csv_parser/csv_parser.go
+++ src/fifa/csv_parser/csv_parser.go
@@ -2,7 +2,7 @@ package ass
 
 import (
 	// "encoding/csv"
-	"fmt"
+
 	"log"
 	"os"
 
@@ -13,19 +13,19 @@ import (
 Parse shit sddf
 */
 func Parse(path string) []map[string]string {
-	fmt.Println("hellow")
 	x, err := os.Open(path)
 	if err != nil {
+		log.Fatal("some failure while opening file", err)
 	}
 	c := csvmap.NewReader(x)
 	cols, errh := c.ReadHeader()
 	if errh != nil {
-		log.Fatal("screw you once more")
+		log.Fatal("some failure while reading csv header", err)
 	}
 	c.Columns = cols
 	data, errcsv := c.ReadAll()
 	if errcsv != nil {
-		log.Fatal("screw you again")
+		log.Fatal("some failure while reading csv file", err)
 	}
 	return data
 }
